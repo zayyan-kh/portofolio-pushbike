a3f700444c60ce5a580dc5639fc29174f62e93f8
zayyan-kh <zayyanwidi22@gmail.com>
2026-01-13 04:07:41 +0000

    Fix: Menambahkan tombol YouTube dan perbaikan desain final

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  // Tambahkan baris ini. Pastikan namanya sama dengan nama repository GitHub Anda
+  // Pastikan nama di bawah ini SAMA PERSIS dengan nama repository Anda di GitHub
   base: '/portofolio-pushbike/', 
 })
